cb82e99074f1875ce3d4cbb166926c5d05953512
tzc1234 <peopli9997@gmail.com>
2022-04-29 09:45:03 +0100

    Initial Commit

---
+++ NetworkingStudy/NetworkingStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingStudyApp.swift
+//  NetworkingStudy
+//
+//  Created by Tsz-Lung on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
